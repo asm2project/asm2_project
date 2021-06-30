d1ff1a7823f4a3065240e42c7751505c38cb3134
xrl111 <77659236+xrl111@users.noreply.github.com>
2021-06-30 12:12:35 +0700

    Net code

---
+++ A2AppDevelop/A2AppDevelop/Models/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace A2AppDevelop.Models
+{
+    public class Account
+    {
+        [Key]
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+
+
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
